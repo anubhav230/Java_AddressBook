723319c281b0f844beab2945da4176b9dcbedfcc
anubhav230 <anubhavsingh230@gmail.com>
2020-06-17 23:59:27 +0530

    ADDED-Able to edit person

--- src/main/java/com/bridgelabz/addressbook/AddressBook.java
+++ src/main/java/com/bridgelabz/addressbook/AddressBook.java
@@ -5,9 +5,38 @@ import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
+    static int count=0;
+    static int flag=1;
+    static Person person = new Person();
+    static Scanner scanner = new Scanner(System.in);
     static List<Person> book=new ArrayList<>();
+    //main methos to start the execution of program
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        while(flag==1) {
+            Scanner scanner2 = new Scanner(System.in);
+
+            System.out.println("Select option: \n 1 = Add person\n 2 = Display\n 3 = Edit person\n 4 = Quit");
+
+            int choice = scanner2.nextInt();
+            switch (choice) {
+                case 1:
+                    addPerson();
+                    break;
+                case 2:
+                    print();
+                    break;
+                case 3:
+                    editPerson();
+                    break;
+                case 4:
+                    flag = 2;
+                    break;
+            }
+        }
+    }
+    //method to add person full details
+    public static void addPerson() {
+
         Person person = new Person();
         System.out.println("Enter first name");
         String firstName = scanner.next();
@@ -28,6 +57,43 @@ public class AddressBook {
         int zip = scanner.nextInt();
         person.setZip(zip);
         book.add(person);
+        count++;
+
+    }
+    //method for printing person details
+    public static void print() {
         System.out.println(book);
+    }
+
+    //method for edit person details by his first name
+    public static void editPerson(){
+        if (count>0) {
+            System.out.println("Enter first name of the person to edit details");
+            String name=scanner.next();
+            boolean isFound=false;
+            int index=0;
+            for (int i=0; i<book.size(); i++) {
+                if (name.equals(book.get(i).getFristName())) {
+                    isFound=true;
+                    index=i;
+                    break;
+                }
+
+            }
+            if (isFound=true) {
+                System.out.println("Enter mobile number");
+                book.get(index).setMobileNumber(scanner.nextLong());
+                System.out.println("Enter State");
+                book.get(index).setState(scanner.next());
+                System.out.println("Enter city");
+                book.get(index).setCity(scanner.next());
+                System.out.println("Enter zip");
+                book.get(index).setZip(scanner.nextInt());
+
+            } else
+                System.out.println("person not exist");
+
+        }
+
     }
 }
